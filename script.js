7afaa398ddff1df3b1720970696da2ac707b6812
rcrxvi <claudetteramos16@gmail.com>
2020-06-13 16:35:36 -0400

    jquery update

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
-//scroll to top on page refresh (PART1)
 window.onbeforeunload = function () {
   window.scrollTo(0, 0);
 }
-//for arrow click part (PART2)
-$( document).ready( function () {
- $('#down').on('click',function () {
+
+$(document).ready(function () {
+ $('.down').on('click',function () {
   $("html").scrollTop(0);
    $('html, body').animate({
-     scrollTop: $(where-you-want-to-scroll).offset().top
+     scrollTop: $("header").offset().top
  }, 1000);
  });
 });
